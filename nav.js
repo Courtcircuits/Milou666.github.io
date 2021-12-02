21fc2cc7f22f7c790e79bf45769f7e52871ae761
Milou <90451752+Milou666@users.noreply.github.com>
2021-12-02 21:49:43 +0100

    Add files via upload

---
+++ nav.js
@@ -0,0 +1,29 @@
+﻿const getRow = document.getElementsByClassName("selectionable");
+let elementSelected = 1;
+
+
+
+function nav(event){
+    if(event.key=="ArrowUp"){
+        
+        if(elementSelected>1){
+            elementSelected--;
+            for(let i=0; i<getRow.length;i++){
+                getRow[i].classList.remove("selected");
+            }
+            getRow[elementSelected-1].classList.add("selected");
+        }
+    }else if(event.key=="ArrowDown"){
+        if(elementSelected<getRow.length){
+            elementSelected++;
+            for(let i=0; i<getRow.length;i++){
+                getRow[i].classList.remove("selected");
+            }
+            getRow[elementSelected-1].classList.add("selected");
+            
+        }
+    }else if(event.key=="Enter"){
+
+    }
+    
+}
